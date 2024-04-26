30187d474db8a2d0d67cf538cb8d4f0dfc677e83
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-04-26 17:45:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var a = 4;
+for (var i = 1; i <= 10; i++) {
+    console.log("".concat(a, " * ").concat(i, "= ") + (a * i));
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+//table of 4 by using for loop
+
+let a:number= 4
+
+for(let i:number= 1; i<=10; i++){
+    console.log(`${a} * ${i}= `+ (a*i))
+}
